b826e6caaaaf4fe47808890936954847426b716d
kikijinqili <jinqi.li.310@gmail.com>
2021-04-11 15:32:41 -0700

    server

--- config/index.js
+++ config/index.js
@@ -2,9 +2,10 @@
  * @Author: Jinqi Li
  * @Date: 2021-04-11 12:02:35
  * @LastEditors: Jinqi Li
- * @LastEditTime: 2021-04-11 15:21:46
+ * @LastEditTime: 2021-04-11 15:31:25
  * @FilePath: /sw-chunyang/config/index.js
  */
-const dev = process.env.NODE_ENV !== 'production';
+// const dev = process.env.NODE_ENV !== 'production';
+// dev ? 'http://localhost:3000' :
 
-export const server ='https://swchunyang.com';
+export const server = 'https://swchunyang.com';
