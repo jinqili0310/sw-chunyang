2dc647f7aa2afc8dca92633c3f9e125c2743ea1e
kikijinqili <jinqi.li.310@gmail.com>
2021-04-11 15:38:20 -0700

    config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
  * @Author: Jinqi Li
  * @Date: 2021-04-06 19:44:50
  * @LastEditors: Jinqi Li
- * @LastEditTime: 2021-04-06 19:45:25
+ * @LastEditTime: 2021-04-11 15:36:55
  * @FilePath: /sw-chunyang/next.config.js
  */
+require('dotenv').config();
+
 module.exports = {
 	webpack(config) {
 		config.module.rules.push({
@@ -13,5 +15,9 @@ module.exports = {
 		});
 
 		return config;
+	},
+	env: {
+		MONGO_URI: process.env.MONGO_URI,
+		NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID
 	}
 };
